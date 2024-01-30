1ed065a924c8b6b043bdfd6a7a50cfc7cc7ec9c5
Guseynov German <gera-60498@mail.ru>
2024-01-30 21:35:23 +0300

    Пытаюсь добавить базу

--- telethon/additional_files/notNeededWords.py
+++ telethon/additional_files/notNeededWords.py
@@ -1,5 +1,37 @@
+import sqlite3
+
+def createbase():
+
+    conn = sqlite3.connect('..\\database\\DBnotNeededWords.db')
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS delete_text (
+        id INTEGER PRIMARY KEY,
+        text_trigger TEXT NOT NULL
+    )
+    """)
+    conn.commit()
+
+
+    # Добавить одно слово
+    # cursor.execute("INSERT INTO delete_text (text_trigger) VALUES (?)", ("asda",))
+    # conn.commit()
+
+    # data = ["#", "vk", "t.me", "Free Gaming"]
+    # for word in data:
+    #     cursor.execute("INSERT INTO delete_text (text_trigger) VALUES (?)", [word]) # можно ещё как (word, )
+    #     conn.commit()
+    # conn.close()
+
+
+
+### старое ###
 DELETE_TEXT = ["#", "vk", "t.me", "Free Gaming."]
 
 STOP_POST = ["дарим", "конкурс", "подпишись", "подписаться", "розыгрыш",]
 
 DELETE_TEXT_SPECIFIC_WORDS = []
+
+if __name__ == "__main__":
+    createbase()
