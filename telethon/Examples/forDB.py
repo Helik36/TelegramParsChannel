31230c6fff80e027f7d67666520f15716f8bda8b
Guseynov German <gera-60498@mail.ru>
2024-02-02 00:33:56 +0300

    Сделал, чтобы переменные по триггер словам брались из БД

--- telethon/Examples/forDB.py
+++ telethon/Examples/forDB.py
@@ -63,6 +63,16 @@ for row in rows:
     print(row[1], " ", row[2])
 """
 
+# Выборка записей
+var_mass = []
+
+cursor.execute("SELECT text_trigger FROM delete_text")
+result = cursor.fetchall()
+for var in result:
+    var_mass.append(var[0])
+print(var_mass)
+# Короткая запись - var_mass = [row[0] for row in cursor.fetchall()]
+
 ########################################################################################################################
 
 # Обновление записи
